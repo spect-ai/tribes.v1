6468b757084775f2b31c46151c62d21bfcac24f2
Aditya Chakraborty <adityachakra16@gmail.com>
2022-02-03 21:40:25 -0600

    Cloud functions for team and invites

---
+++ app/scripts/cloudFunction.js
@@ -0,0 +1,249 @@
+Moralis.Cloud.define("getOrCreateUser", async (request) => {
+  const logger = Moralis.Cloud.getLogger();
+
+  try {
+    const userInfoQuery = new Moralis.Query("UserInfo");
+    userInfoQuery.equalTo("ethAddress", request.params.ethAddress);
+    var userInfo = await userInfoQuery.first();
+
+    if (!userInfo) {
+      userInfo = new Moralis.Object("UserInfo");
+      //userInfo.set("ethAddress", request.user.get("ethAddress"));
+      userInfo.set("ethAddress", request.params.ethAddress);
+      userInfo.set("teams", []);
+      await Moralis.Object.saveAll([userInfo], { useMasterKey: true });
+    }
+
+    return userInfo;
+  } catch (err) {
+    logger.error(`Error while creating team ${err}`);
+    return false;
+  }
+});
+
+Moralis.Cloud.define("createTeam", async (request) => {
+  const logger = Moralis.Cloud.getLogger();
+  try {
+    const teamQuery = new Moralis.Query("Team");
+    teamQuery.notEqualTo("objectId", "");
+    const teamCount = await teamQuery.count();
+
+    const team = new Moralis.Object("Team");
+    const teamId = teamCount + 1;
+    team.set("teamId", teamId);
+    team.set("name", request.params.name);
+    team.set("mission", request.params.mission);
+    team.set("address", request.params.teamAddress);
+    team.set("treasuryAddress", request.params.treasuryAddress);
+    team.set("onchain", false);
+    //team.set("members", [{ ethAddress: request.user.get("ethAddress"), role: "admin" }]);
+    team.set("members", [{ ethAddress: request.params.ethAddress, role: "admin" }]);
+    team.set("organization", request.params.organization);
+    team.set("organizationVerified", false);
+    team.set("openApplications", request.params.openApplications);
+    team.set("applicationRequirements", request.params.applicationRequirements);
+
+    const userInfoQuery = new Moralis.Query("UserInfo");
+    //userInfoQuery.equalTo("ethAddress", request.user.get("ethAddress"))
+    userInfoQuery.equalTo("ethAddress", request.params.ethAddress);
+    const userInfo = await userInfoQuery.first();
+    teamMemberships = userInfo.get("teams").concat([teamId]);
+    userInfo.set("teams", teamMemberships);
+    await Moralis.Object.saveAll([team, userInfo], { useMasterKey: true });
+
+    return team;
+  } catch (err) {
+    logger.error(`Error while creating team ${err}`);
+    return false;
+  }
+});
+
+Moralis.Cloud.define("updateTeam", async (request) => {
+  const logger = Moralis.Cloud.getLogger();
+  try {
+    const teamQuery = new Moralis.Query("Team");
+    teamQuery.equalTo("teamId", request.params.teamId);
+    var team = await teamQuery.first();
+
+    //const canUpdate = await hasAccess(request.user.get("ethAddress"), team, (requiredAccess = "admin"));
+    const canUpdate = await hasAccess(request.params.ethAddress, team, (requiredAccess = "admin"));
+    if (canUpdate) {
+      team.set("name", request.params.name);
+      team.set("mission", request.params.mission);
+      team.set("treasuryAddress", request.params.treasuryAddress);
+      team.set("organization", request.params.organization);
+      team.set("openApplications", request.params.openApplications);
+      team.set("applicationRequirements", request.params.applicationRequirements);
+
+      await Moralis.Object.saveAll([team], { useMasterKey: true });
+    }
+
+    return team;
+  } catch (err) {
+    logger.error(`Error while updating team ${err}`);
+    return false;
+  }
+});
+
+async function hasAccess(ethAddress, team, requiredAccess) {
+  const members = team.get("members");
+  for (var member of members) {
+    if (member["ethAddress"] === ethAddress) {
+      return member["role"] === requiredAccess;
+    }
+  }
+  return false;
+}
+
+Moralis.Cloud.define("updateMembers", async (request) => {
+  /*request.params.members: [{"ethAddress":"0x232324", "role": "admin", "updateType":"invite"},
+                              {"ethAddress':'0x232324', 'role': 'admin', 'updateType':'revoke'}, 
+                              {'ethAddress':'0x232324', 'newRole': 'admin', 'oldRole':'general', 'updateType':'roleChange'}, 
+                              {'ethAddress':'0x232324', 'role': 'admin', 'updateType':'invite']
+      */
+  const logger = Moralis.Cloud.getLogger();
+  try {
+    const teamQuery = new Moralis.Query("Team");
+    teamQuery.equalTo("teamId", request.params.teamId);
+    var team = await teamQuery.first();
+    const canUpdate = await hasAccess(request.params.ethAddress, team, (requiredAccess = "admin"));
+    if (canUpdate) {
+      var invitedMembers = request.params.members.filter((m) => m.updateType === "invite");
+      logger.info(`invitedMembers: ${JSON.stringify(invitedMembers)}`);
+
+      var revokedMemberAddresses = [];
+      var revokedMembers = request.params.members.filter((m) => m.updateType === "revoke");
+      revokedMembers.map((a) => revokedMemberAddresses.push(a.ethAddress));
+      logger.info(`revokedMemberAddresses: ${JSON.stringify(revokedMemberAddresses)}`);
+
+      var roleChangedMembers = request.params.members.filter((m) => m.updateType === "roleChange");
+      //invite(invitedMembers, request.params.teamId, request.user.get("ethAddress"));
+
+      await invite(invitedMembers, request.params.teamId, request.params.ethAddress);
+      await revoke(revokedMemberAddresses, request.params.teamId);
+
+      return true;
+    } else {
+      //logger.info(`User ${request.user.get("ethAddress")} doesnt have access to update member roles`);
+      logger.info(`User ${request.params.ethAddress} doesnt have access to update member roles`);
+      return false;
+    }
+  } catch (err) {
+    logger.error(`Error while creating team ${err}`);
+    return false;
+  }
+});
+
+async function invite(members, teamId, invitedBy) {
+  //members: [{'ethAddress':'0x232324', 'role': 'admin'}]
+  const logger = Moralis.Cloud.getLogger();
+  var invites = [];
+  logger.info(`Members to invite ${JSON.stringify(members)}`);
+  for (var member of members) {
+    const invitationQuery = new Moralis.Query("Invitation");
+    invitationQuery.equalTo("teamId", teamId);
+    invitationQuery.equalTo("ethAddress", member["ethAddress"]);
+    const invited = await invitationQuery.first();
+    if (!invited && member["ethAddress"] !== invitedBy) {
+      const invitation = new Moralis.Object("Invitation");
+      invitation.set("ethAddress", member["ethAddress"]);
+      invitation.set("role", member["role"]);
+      invitation.set("invitedBy", invitedBy);
+      invitation.set("active", true);
+      invitation.set("teamId", teamId);
+
+      invites.push(invitation);
+    }
+  }
+  logger.info(`invites: ${JSON.stringify(invites)}`);
+
+  await Moralis.Object.saveAll(invites, { useMasterKey: true });
+}
+
+async function revoke(members, teamId) {
+  //members: Set of members
+  const teamQuery = new Moralis.Query("Team");
+  teamQuery.equalTo("teamId", teamId);
+  var team = await teamQuery.first();
+  var membersInTeam = team?.get("members");
+  var i = 0;
+  for (var member of membersInTeam) {
+    if (members.includes(member["ethAddress"])) {
+      membersInTeam.splice(i, 1);
+    } else {
+      ++i;
+    }
+  }
+  team.set("members", membersInTeam);
+  logger.info(`team after revokedMembers: ${JSON.stringify(team)}`);
+
+  await Moralis.Object.saveAll([team], { useMasterKey: true });
+}
+
+Moralis.Cloud.define("getMyInvites", async (request) => {
+  try {
+    const logger = Moralis.Cloud.getLogger();
+    const invitationQuery = new Moralis.Query("Invitation");
+    //invitationQuery.equalTo("ethAddress", request.user.get("ethAddress"));
+    invitationQuery.equalTo("ethAddress", request.params.ethAddress);
+    invitationQuery.equalTo("active", true);
+    return await invitationQuery.find();
+  } catch (err) {
+    logger.error(`Error whilte creating team ${err}`);
+    return false;
+  }
+});
+
+Moralis.Cloud.define("acceptInvite", async (request) => {
+  try {
+    const logger = Moralis.Cloud.getLogger();
+    // Set invitation to inactive
+    const invitationQuery = new Moralis.Query("Invitation");
+    //invitationQuery.equalTo("ethAddress", request.user.get("ethAddress"));
+    invitationQuery.equalTo("ethAddress", request.params.ethAddress);
+    invitationQuery.equalTo("teamId", request.params.teamId);
+    invitationQuery.equalTo("active", true);
+    const invitation = await invitationQuery.first();
+    if (invitation) {
+      invitation.set("active", false);
+
+      // Add team in user's profile
+      const userInfoQuery = new Moralis.Query("UserInfo");
+      userInfoQuery.equalTo("ethAddress", request.params.ethAddress);
+      var userInfo = await userInfoQuery.first();
+      var userTeams = userInfo.get("teams").concat([request.params.teamId]);
+      logger.info(`user teams ${JSON.stringify(userTeams)}`);
+      userInfo.set("teams", userTeams);
+
+      // Add user and role to team members
+      const teamQuery = new Moralis.Query("Team");
+      teamQuery.equalTo("teamId", request.params.teamId);
+      const team = await teamQuery.first();
+      const members = team.get("members");
+      const member = [{ ethAddress: request.params.ethAddress, role: invitation.get("role") }];
+      team.set("members", members.concat(member));
+
+      await Moralis.Object.saveAll([invitation, userInfo, team], { useMasterKey: true });
+
+      return true;
+    } else {
+      logger.info(`Not invited to this team`);
+      return false;
+    }
+  } catch (err) {
+    logger.error(`Error whilte creating team ${err}`);
+    return false;
+  }
+});
+
+async function getTeam(teamId) {
+  const teamQuery = new Moralis.Query("Team");
+  teamQuery.equalTo("teamId", teamId);
+  return await teamQuery.first();
+}
+
+Moralis.Cloud.define("getTeam", async (request) => {
+  const logger = Moralis.Cloud.getLogger();
+
+  return await getTeam(request.params.teamId);
+});
