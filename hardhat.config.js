f402fb1ef1cbafa4673e5f751c927ea660363414
Aditya Chakraborty <adityachakra16@gmail.com>
2022-02-26 16:06:45 -0600

    Add networks to hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,21 +1,82 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+// The next line is part of the sample project, you don't need it in your
+// project. It imports a Hardhat task definition, that can be used for
+// testing the frontend.
+// require("./tasks/faucet");
+// const fs = require("fs");
+// const privateKey = fs.readFileSync(".secret").toString().trim();
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
-  solidity: "0.8.4",
+  defaultNetwork: "hardhat",
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: "",
+  },
+
+  networks: {
+    hardhat: {
+      inject: false, // optional. If true, it will EXPOSE your mnemonic in your frontend code. Then it would be available as an "in-page browser wallet" / signer which can sign without confirmation.
+      accounts: {
+        mnemonic: "test test test test test test test test test test test junk", // test test test test test test test test test test test junk
+      },
+      chainId: 1337,
+    },
+    mumbai: {
+      url: "https://speedy-nodes-nyc.moralis.io/27686f41b7c9afc73b87dfa2/polygon/mumbai",
+      network_id: 80001,
+      confirmations: 2,
+      accounts: [],
+    },
+    polygon: {
+      url: "https://speedy-nodes-nyc.moralis.io/f84f46508f22a737cbbdb355/polygon/mainnet",
+      network_id: 137,
+      confirmations: 2,
+      accounts: {
+        mnemonic: "secret",
+      },
+    },
+    fuji: {
+      url: "https://speedy-nodes-nyc.moralis.io/27686f41b7c9afc73b87dfa2/avalanche/testnet",
+      network_id: 43113,
+      confirmations: 2,
+      accounts: {
+        mnemonic: "secret",
+      },
+    },
+    avalanche: {
+      url: "https://speedy-nodes-nyc.moralis.io/27686f41b7c9afc73b87dfa2/avalanche/mainnet",
+      network_id: 43114,
+      confirmations: 2,
+      accounts: {
+        mnemonic: "secret",
+      },
+    },
+    rinkeby: {
+      url: "https://speedy-nodes-nyc.moralis.io/27686f41b7c9afc73b87dfa2/eth/rinkeby",
+      network_id: 4,
+      confirmations: 2,
+      accounts: {
+        mnemonic: "secret",
+      },
+    },
+    ethereum: {
+      url: "https://speedy-nodes-nyc.moralis.io/27686f41b7c9afc73b87dfa2/eth/mainnet",
+      network_id: 1,
+      confirmations: 2,
+      accounts: {
+        mnemonic: "secret",
+      },
+    },
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
 };
