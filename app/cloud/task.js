d00af421764b97f1a020a5c047567b26f2c2877b
Aditya Chakraborty <adityachakra16@gmail.com>
2022-03-29 23:39:37 -0500

    Filter out null reward values and invalid networks in batch pay

--- app/cloud/task.js
+++ app/cloud/task.js
@@ -38,7 +38,6 @@ async function getTaskObjByBoardId(boardId) {
         description: 0,
         submission: 0,
         activity: 0,
-        chain: 0,
       },
     },
   ];
@@ -776,7 +775,7 @@ Moralis.Cloud.define("getBatchPayInfo", async (request) => {
     for (var task of tasks) {
       var assignee = task.get("assignee");
 
-      if (assignee?.length > 0 && task.get("value") > 0) {
+      if (assignee?.length > 0 && task.get("value") && task.get("value") > 0) {
         var contributor = task.get("assignee")[0];
         var value = task.get("value");
         var token = task.get("token");
