822f1d950e5b2eba812ed56f0d2da0d75466c0e6
Aditya Veer <nakulparmar15@gmail.com>
2022-03-31 22:25:43 +0530

    trying different redirect uri

--- pages/api/auth/discord/user/login.ts
+++ pages/api/auth/discord/user/login.ts
@@ -17,7 +17,7 @@ export default async function handler(
       client_secret: "KlYQynvlUWjfWVlvh0BI8zfqrivf4CtC",
       code: req.query.code as string,
       grant_type: "authorization_code",
-      redirect_uri: `https://dev.spect.network/`,
+      redirect_uri: `https://dev.spect.network/api/auth/discord/user/login`,
       scope: "identify guilds",
     }),
     headers: {
