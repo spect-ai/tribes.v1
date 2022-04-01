f0e4b2697d911cd9f3bf3dd95fefb17bac9d3b99
Aditya Chakraborty <adityachakra16@gmail.com>
2022-04-01 01:15:27 -0500

    Approval bugfixes on payment modal

--- app/cloud/user.js
+++ app/cloud/user.js
@@ -116,18 +116,6 @@ Moralis.Cloud.define("getOrCreateUser", async (request) => {
         request.user.get("ethAddress")
       );
       request.user.set("username", `fren${userCount}`);
-      request.user.set("distributorApproved", {
-        1: [],
-        4: [],
-        137: [],
-        80001: [],
-        43113: [],
-        43114: [],
-        250: [],
-        42161: [],
-        100: [],
-        10: [],
-      });
 
       await Moralis.Object.saveAll([userInfo, request.user], {
         useMasterKey: true,
